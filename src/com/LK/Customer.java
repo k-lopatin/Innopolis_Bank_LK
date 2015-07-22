070c5f682f13cdbb3d28ca636e9f07c23e4f33f7
Gosha Krikun <gosha@krikun.org>
2015-07-22 12:26:24 +0400

    add Customer constructor

--- src/com/LK/Customer.java
+++ src/com/LK/Customer.java
@@ -1,14 +1,45 @@
 package com.LK;
 
+import java.util.Date;
+
 /**
  * Created by lk1195 on 22.07.15.
  */
 
 public class Customer implements Transactions {
+    private int id;
     private String name;
+    private String surname;
+    private Date birthDate;
 
-    public void Customer(String name) {
+    /**
+     * male - is true
+     * female - false
+     */
+    private boolean sexMale;
+
+    private String document;
+    private int accountType;
+    private String[] log;
+
+    /**
+     * standard constructor for required fields
+     * @param id
+     * @param name
+     * @param surname
+     * @param birthDate
+     * @param sexMale
+     * @param document
+     * @param accountType
+     */
+    public Customer(int id, String name, String surname, Date birthDate, boolean sexMale, String document, int accountType) {
+        this.id = id;
         this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.sexMale = sexMale;
+        this.document = document;
+        this.accountType = accountType;
     }
 
     @Override
