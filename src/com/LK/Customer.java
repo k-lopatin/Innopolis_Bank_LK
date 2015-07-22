c8eedd99b49fa2bf2d24fd7ce57023a52e95518d
lk1195 <lk1195@ya.ru>
2015-07-22 11:00:29 +0300

    initial commit

---
+++ src/com/LK/Customer.java
@@ -0,0 +1,8 @@
+package com.LK;
+
+/**
+ * Created by lk1195 on 22.07.15.
+ */
+public class Customer {
+
+}
---
+++ src/com/LK/Transactions.java
@@ -0,0 +1,13 @@
+package com.LK;
+/**
+
+ */
+public interface Transactions {
+
+    void withdraw(double sum);
+
+    void deposit(double sum);
+
+    void payInterests();
+
+}
