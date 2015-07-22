c502502422d760642f263c167645b7f85c363ae2
Gosha Krikun <gosha@krikun.org>
2015-07-22 12:12:44 +0400

    add Customer class

--- src/com/LK/Customer.java
+++ src/com/LK/Customer.java
@@ -3,6 +3,26 @@ package com.LK;
 /**
  * Created by lk1195 on 22.07.15.
  */
-public class Customer {
 
+public class Customer implements Transactions {
+    private String name;
+
+    public void Customer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void withdraw(double sum) {
+
+    }
+
+    @Override
+    public void deposit(double sum) {
+
+    }
+
+    @Override
+    public void payInterests() {
+
+    }
 }
